65bb982ed7abead334bb11bc94df0c68885e9759
Nathanael <david.rawatan@gmail.com>
2022-04-21 15:30:15 +0700

    Added middleware and session

--- app.js
+++ app.js
@@ -3,11 +3,21 @@
 const express = require("express")
 const app = express()
 const port = 3000
+const session = require("express-session")
+
 const routes = require("./routes")
 
 app.set("view engine", "ejs")
 app.use(express.urlencoded({extended:false}))
-
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { 
+        secure: false,
+        sameSite: true
+    }
+}))
 app.use("/", routes)
 
 app.listen(port, () =>{
