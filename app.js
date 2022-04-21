3ee288738a58fc492b40bee1d05ac2c97ecc76d5
Kevin Samuel <kevinsbl3nd@gmail.com>
2022-04-21 10:43:04 +0700

    Create first structure

---
+++ app.js
@@ -0,0 +1,13 @@
+"use strict"
+
+const express = require("express")
+const app = express()
+const port = 3000
+const routes = require("./routes")
+
+app.set("view engine", "ejs")
+app.use(express.urlencoded({extended:false}))
+
+app.listen(port, () =>{
+    console.log(`Listening to port : ${port}`)
+})
---
+++ controllers/index.js
@@ -0,0 +1,10 @@
+"use strict"
+const { User, Profile } = require("../models")
+
+class Controller {
+    static read(req, res) {
+
+    }
+}
+
+module.exports = Controller
